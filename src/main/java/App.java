95457a08b70267979622c14f937cab0612009b4b
KhanZuh <khanzuhair96@live.co.uk>
2024-06-06 12:22:37 +0100

    add to line 6 on main

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void manin(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Added this on line 4");
-        
+
+        System.out.println("Added this to line 6 on main branch");
     }
 }
